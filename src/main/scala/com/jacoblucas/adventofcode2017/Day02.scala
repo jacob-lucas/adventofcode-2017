14d3f4684358214aaa4d066995e945fd855de709
Jacob Lucas <jacob.lucas@grab.com>
2017-12-01 21:30:55 -0800

    Day 2 part 2

--- src/main/scala/com/jacoblucas/adventofcode2017/Day02.scala
+++ src/main/scala/com/jacoblucas/adventofcode2017/Day02.scala
@@ -3,6 +3,24 @@ package com.jacoblucas.adventofcode2017
 object Day02 {
   def checksum(input: List[Int]): Int = input.max - input.min
 
+  def division(input: List[Int]): Int = {
+    val pairs = for (
+      i <- input.indices;
+      j <- input.indices if i != j
+    ) yield {
+      val max = Math.max(input(i), input(j))
+      val min = Math.min(input(i), input(j))
+
+      (max, min)
+    }
+
+    pairs
+      .distinct
+      .filter(p => p._1 % p._2 == 0)
+      .map(p => p._1 / p._2)
+      .head
+  }
+
   def main(args: Array[String]): Unit = {
     val lines = Util.read("/Day02.txt")
     val rows = lines.map(l => {
@@ -16,5 +34,6 @@ object Day02 {
     })
 
     println(rows.map(r => checksum(r.toList)).sum)
+    println(rows.map(r => division(r.toList)).sum)
   }
 }
--- src/test/scala/com/jacoblucas/adventofcode2017/Day02Test.scala
+++ src/test/scala/com/jacoblucas/adventofcode2017/Day02Test.scala
@@ -14,5 +14,10 @@ class Day02Test extends WordSpec with Matchers {
         assertThat(Day02.checksum(List(5,1,9,5)), is(8))
       }
     }
+    "calculating division for a line" should {
+      "return the result of dividing the only two numbers that evenly divide" in {
+        assertThat(Day02.division(List(5,9,2,8)), is(4))
+      }
+    }
   }
 }
