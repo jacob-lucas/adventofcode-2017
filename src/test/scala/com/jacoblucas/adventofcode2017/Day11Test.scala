e07c9dc7b9d16520e09a3106257d7f1dac38f72b
Jacob Lucas <jacob.lucas@grab.com>
2017-12-12 15:23:03 -0800

    Day 11 part 2

--- src/test/scala/com/jacoblucas/adventofcode2017/Day11Test.scala
+++ src/test/scala/com/jacoblucas/adventofcode2017/Day11Test.scala
@@ -11,13 +11,13 @@ class Day11Test extends WordSpec with Matchers {
   "Day11" when {
     "calculating steps away" should {
       "calculate correctly" in {
-        assertThat(Day11.stepsAway(List("ne", "ne", "ne")), is(3))
-        assertThat(Day11.stepsAway(List("ne", "ne", "sw", "sw")), is(0))
-        assertThat(Day11.stepsAway(List("se", "nw", "se", "nw")), is(0))
-        assertThat(Day11.stepsAway(List("se", "ne", "se", "ne")), is(4))
-        assertThat(Day11.stepsAway(List("ne", "ne", "s", "s")), is(2))
-        assertThat(Day11.stepsAway(List("s", "ne", "n", "nw", "sw", "s", "se")), is(1))
-        assertThat(Day11.stepsAway(List("se", "sw", "se", "sw", "sw")), is(3))
+        assertThat(Day11.stepsAway(List("ne", "ne", "ne"))._1, is(3))
+        assertThat(Day11.stepsAway(List("ne", "ne", "sw", "sw"))._1, is(0))
+        assertThat(Day11.stepsAway(List("se", "nw", "se", "nw"))._1, is(0))
+        assertThat(Day11.stepsAway(List("se", "ne", "se", "ne"))._1, is(4))
+        assertThat(Day11.stepsAway(List("ne", "ne", "s", "s"))._1, is(2))
+        assertThat(Day11.stepsAway(List("s", "ne", "n", "nw", "sw", "s", "se"))._1, is(1))
+        assertThat(Day11.stepsAway(List("se", "sw", "se", "sw", "sw"))._1, is(3))
       }
     }
   }
