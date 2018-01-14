13cc00c020f921c039314d5ed621fe37f39526f4
Jacob Lucas <jacob.lucas@grab.com>
2018-01-14 00:28:37 -0800

    Day 15 part 2

--- src/main/scala/com/jacoblucas/adventofcode2017/Day15.scala
+++ src/main/scala/com/jacoblucas/adventofcode2017/Day15.scala
@@ -1,7 +1,11 @@
 package com.jacoblucas.adventofcode2017
 
-case class Generator(id: String, factor: Int, seed: Int) {
-  val iterator: Iterator[Int] = Iterator.iterate(seed)(v => ((v.toLong * factor) % 2147483647).toInt).drop(1)
+case class Generator(id: String, factor: Int, seed: Int, valueFilter: (Int) => Boolean) {
+  val iterator: Iterator[Int] =
+    Iterator.iterate(seed)(v => ((v.toLong * factor) % 2147483647).toInt)
+      .drop(1)
+      .filter(valueFilter)
+
   def generate(): Int = iterator.next()
 }
 
@@ -11,12 +15,21 @@ object Day15 {
     pairedIterator.take(n).filter(p => p._1.toBinaryString.takeRight(16) == p._2.toBinaryString.takeRight(16))
   }
 
-  def main(args: Array[String]): Unit = {
-    val genA = Generator("A", 16807, 116)
-    val genB = Generator("B", 48271, 299)
+  def always(n: Int): Boolean = true
+  def divBy4(n: Int): Boolean = n % 4 == 0
+  def divBy8(n: Int): Boolean = n % 8 == 0
 
+  def main(args: Array[String]): Unit = {
     // part 1
-    val matches = findMatchingPairs(genA, genB, 40000000)
-    println(matches.size)
+    val genA = Generator("A", 16807, 116, always)
+    val genB = Generator("B", 48271, 299, always)
+    val matches1 = findMatchingPairs(genA, genB, 40000000)
+    println(matches1.size)
+
+    // part 2
+    val genC = Generator("A", 16807, 116, divBy4)
+    val genD = Generator("B", 48271, 299, divBy8)
+    val matches2 = findMatchingPairs(genC, genD, 5000000)
+    println(matches2.size)
   }
 }
--- src/test/scala/com/jacoblucas/adventofcode2017/Day15Test.scala
+++ src/test/scala/com/jacoblucas/adventofcode2017/Day15Test.scala
@@ -8,12 +8,11 @@ import org.scalatest.{Matchers, WordSpec}
 
 @RunWith(classOf[JUnitRunner])
 class Day15Test extends WordSpec with Matchers {
-
   "Day15" when {
     "generating a next value" should {
       "generate correctly" in {
-        val genA = Generator("A", 16807, 65)
-        val genB = Generator("B", 48271, 8921)
+        val genA = Generator("A", 16807, 65, Day15.always)
+        val genB = Generator("B", 48271, 8921, Day15.always)
 
         assertThat(genA.generate(), is(1092455))
         assertThat(genA.generate(), is(1181022009))
@@ -27,11 +26,27 @@ class Day15Test extends WordSpec with Matchers {
         assertThat(genB.generate(), is(137874439))
         assertThat(genB.generate(), is(285222916))
       }
+      "generate with a value filter correctly" in {
+        val genA = Generator("A", 16807, 65, Day15.divBy4)
+        val genB = Generator("B", 48271, 8921, Day15.divBy8)
+
+        assertThat(genA.generate(), is(1352636452))
+        assertThat(genA.generate(), is(1992081072))
+        assertThat(genA.generate(), is(530830436))
+        assertThat(genA.generate(), is(1980017072))
+        assertThat(genA.generate(), is(740335192))
+
+        assertThat(genB.generate(), is(1233683848))
+        assertThat(genB.generate(), is(862516352))
+        assertThat(genB.generate(), is(1159784568))
+        assertThat(genB.generate(), is(1616057672))
+        assertThat(genB.generate(), is(412269392))
+      }
     }
     "detecting matched pairs" should {
       "count a pair when the lowest 16 bits of the binary string matches" in {
-        val genA = Generator("A", 16807, 65)
-        val genB = Generator("B", 48271, 8921)
+        val genA = Generator("A", 16807, 65, Day15.always)
+        val genB = Generator("B", 48271, 8921, Day15.always)
         val matches = Day15.findMatchingPairs(genA, genB, 5)
         assertThat(matches.next(), is((245556042, 1431495498)))
       }
